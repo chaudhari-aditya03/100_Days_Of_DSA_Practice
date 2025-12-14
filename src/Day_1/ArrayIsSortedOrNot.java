8251e17edf006a642010e31170e309c898ec429a
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-12-14 22:36:00 +0530

    Day_1 d

---
+++ src/Day_1/ArrayIsSortedOrNot.java
@@ -0,0 +1,27 @@
+package Day_1;
+
+import java.util.Scanner;
+
+public class ArrayIsSortedOrNot {
+    static String isSorted(int[] arr){
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]>arr[j]) return "Array is Not Sorted";
+            }
+        }
+        return "Array is Sorted";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements in the array");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(isSorted(arr));
+    }
+}
